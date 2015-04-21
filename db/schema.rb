27b18d5e34a783d056b31599647cace923922bcc
masterdefy <masteridefy@gmail.com>
2015-04-21 11:04:07 +0300

    Issue API.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409130151) do
+ActiveRecord::Schema.define(version: 20150421075536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "issues", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "issues", ["project_id"], name: "index_issues_on_project_id", using: :btree
 
   create_table "projects", force: true do |t|
     t.string   "name"
